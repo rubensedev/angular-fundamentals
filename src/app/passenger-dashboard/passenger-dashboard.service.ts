cc169bf97a976c2298201bc85351d801351c1e84
Ruben Sedeño Rodriguez <ruben.sedeno@ceceu.junta-andalucia.es>
2022-08-01 13:36:50 +0200

    finished services, http and observables

--- src/app/passenger-dashboard/passenger-dashboard.service.ts
+++ src/app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 // we need to import Observable and map to use it below
 // because we are goig to map through an obervable not an array
@@ -8,7 +8,8 @@ import { map } from 'rxjs/operators';
 
 import { Passenger } from './models/passenger.interface';
 
-const PASSENGER_API: string = '/assets/db.json';
+// const PASSENGER_API: string = '/assets/db.json';
+// we create a new url api to use it with the library angular-in-memory-web-api
 const PASSENGER_API2: string = 'api/passengers';
 
 @Injectable()
@@ -23,34 +24,9 @@ export class PassengerDashboardService {
       .get<Passenger[]>(PASSENGER_API2)
       .pipe(map((response) => response));
   }
-  // getPassengers(): Observable<Passenger[]> {
-  //   // before we had here the whole passengers collection
-  //   // with get we type the object we are goig to "get"
-  //   return this.httpClient
-  //     .get<{ passengers: Passenger[] }>(PASSENGER_API2)
-  //     .pipe(
-  //       map((response) => {
-  //         console.log(response);
-  //         return response;
-  //       })
-  //     );
-  // }
-  // updatePassengers(passenger: Passenger): Observable<Passenger> {
-  //   // before we had here the whole passengers collection
-  //   return this.httpClient
-  //     .put(`${JSON.stringify(PASSENGER_API)}/${passenger.id}`, passenger)
-  //     .pipe(map((response: any) => response.passengers));
-  // }
 
   // we return a null becaus the library used is configure in that way, because is a local database
   updatePassengers(passenger: Passenger): Observable<null> {
-    // before we had here the whole passengers collection
-    // const httpOptions = {
-    //   headers: new HttpHeaders({
-    //     'Content-Type': 'application/json',
-    //   }),
-    // };
-
     return this.httpClient.put<null>(
       `${PASSENGER_API2}/${passenger.id}`,
       passenger
@@ -58,13 +34,7 @@ export class PassengerDashboardService {
   }
 
   deletePassenger(id: number): Observable<Passenger> {
-    // before we had here the whole passengers collection
-    // const httpOptions = {
-    //   headers: new HttpHeaders({
-    //     'Content-Type': 'application/json',
-    //   }),
-    // };
-
+    // here we don't need to pass the passenger, only the url
     return this.httpClient.delete<Passenger>(`${PASSENGER_API2}/${id}`);
   }
 }
